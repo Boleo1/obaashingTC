a7891d16f77b721fe5098d47d3fc9cb96314fc48
boleo <118724888+Boleo1@users.noreply.github.com>
2024-08-11 20:31:32 -0500

    first commit for project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+  <x-slot:heading>
+    Home
+  </x-slot:heading>
+  <h2>About</h2>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+<x-slot:heading>
+  Home
+</x-slot:heading>
+<h2>Home</h2>
+</x-layout>
